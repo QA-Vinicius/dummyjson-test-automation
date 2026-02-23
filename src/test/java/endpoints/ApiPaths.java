1c386494ec31b867a80820df999a556512f0e093
vinicius0alves <viniciusmartalves@gmail.com>
2026-02-23 04:03:17 -0300

    feat: Criando o pacote endpoints e classe ApiPaths para armazenar os endpoints em variáveis

---
+++ src/test/java/endpoints/ApiPaths.java
@@ -0,0 +1,10 @@
+package endpoints;
+
+public class ApiPaths {
+    public static final String TEST = "/test";
+    public static final String USERS = "/users";
+    public static final String AUTH_LOGIN = "/auth/login";
+    public static final String AUTH_PRODUCTS = "/auth/products";
+    public static final String PRODUCTS = "/products";
+    public static final String PRODUCTS_ADD = "/products/add";
+}
