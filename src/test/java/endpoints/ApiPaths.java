a22c34e1b5636def8d2533048ceb36deb2ec016d
vinicius0alves <viniciusmartalves@gmail.com>
2026-02-23 10:33:37 -0300

    test: adiciona testes para GET /users

--- src/test/java/endpoints/ApiPaths.java
+++ src/test/java/endpoints/ApiPaths.java
@@ -3,6 +3,8 @@ package endpoints;
 public class ApiPaths {
     public static final String TEST = "/test";
     public static final String USERS = "/users";
+    public static final String USERS_ID = "/users/{id}";
+    public static final String USERS_SEARCH = "/users/search";
     public static final String AUTH_LOGIN = "/auth/login";
     public static final String AUTH_PRODUCTS = "/auth/products";
     public static final String PRODUCTS = "/products";
